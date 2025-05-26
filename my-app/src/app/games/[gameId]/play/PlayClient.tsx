3a331fd37b114b7c23f6968574954154bfb00929
edgarLB <103465521+edgarLB@users.noreply.github.com>
2025-05-25 17:32:32 -0700

    Display both game boards
    The game screen now just displays the grid of cards for both the opponent and self.

--- my-app/src/app/games/[gameId]/play/PlayClient.tsx
+++ my-app/src/app/games/[gameId]/play/PlayClient.tsx
@@ -1,6 +1,8 @@
 "use client"
 import {getLocalPlayerId} from "@/lib/utils";
 import {useState} from "react";
+import {getPublicUrl} from "@/lib/getPublicUrl";
+import CardBackIMG from "@/assets/images/back_temp.webp"
 
 export default function PlayClient({game, players, cards}){
 
@@ -11,6 +13,33 @@ export default function PlayClient({game, players, cards}){
     return(
         <div>
             <h2>{isMyTurn ? "My Turn" : "Opponent Turn"}</h2>
+            {/*    My Board    */}
+            <div className="grid grid-cols-6 gap-2">
+                {cards.map(c => (
+                    <button
+                        key={c.id}
+                    >
+                        <img
+                            src={getPublicUrl(c.image)}
+                            alt={c.name}
+                        />
+                    </button>
+                ))}
+            </div>
+
+            {/*    Enemy's Board    */}
+            <div className="grid grid-cols-6 gap-2">
+                {cards.map(c => (
+                    <button
+                        key={c.id}
+                    >
+                        <img
+                            src="/images/back_temp.webp"
+                            alt="Flipped Card"
+                        />
+                    </button>
+                ))}
+            </div>
         </div>
     )
 }
