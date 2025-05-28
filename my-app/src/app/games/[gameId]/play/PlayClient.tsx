ed06dfe0414cb0fd74d4b85042eed3244c25febd
edgarLB <103465521+edgarLB@users.noreply.github.com>
2025-05-27 22:40:40 -0700

    Players can end their turn

--- my-app/src/app/games/[gameId]/play/PlayClient.tsx
+++ my-app/src/app/games/[gameId]/play/PlayClient.tsx
@@ -1,18 +1,66 @@
 "use client"
 import {getLocalPlayerId} from "@/lib/utils";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {getPublicUrl} from "@/lib/getPublicUrl";
 import CardBackIMG from "@/assets/images/back_temp.webp"
+import {supabase} from "@/lib/supabaseClient";
+import {Button} from "@/components/ui/button";
 
 export default function PlayClient({game, players, cards}){
 
     const localPlayerId = getLocalPlayerId();
+    const opponentId = players.find((p) => p.id !== localPlayerId)?.id;
     const [currentTurnId, setCurrentTurnId] = useState(game.current_player_id);
     const isMyTurn = currentTurnId === localPlayerId;
+
+    useEffect(() => {
+        // Realtime games channel
+        const gameChannel = supabase
+            .channel(`game-${game.id}`)
+            .on(
+                "postgres_changes",
+                {
+                    event: "UPDATE",
+                    schema: "public",
+                    table: "games",
+                    filter: `id=eq.${game.id}`,
+                },
+                (payload) => {
+                    // When turn changes in DB, update the local state
+                    const newTurnId = payload.new.current_player_id;
+                    setCurrentTurnId(newTurnId);
+                }
+            )
+            .subscribe();
+
+        return () => supabase.removeChannel(gameChannel);
+    }, [game.id]);
+
+    async function endTurn() {
+        // Safeguard: Only current player can end their turn
+        if (!isMyTurn || !opponentId) return;
+
+        // Update DB current_player_id to opponentID
+        const { error} =await supabase
+            .from("games")
+            .update({"current_player_id": opponentId})
+            .eq("id", game.id)
+            .single();
+
+        // Only update local state if DB updated successfully
+        if( !error) {
+            setCurrentTurnId(opponentId);
+        }
+        else {
+            console.log("Did not end turn:", error);
+        }
+    }
 
     return(
         <div>
             <h2>{isMyTurn ? "My Turn" : "Opponent Turn"}</h2>
+            <Button disabled={!isMyTurn} onClick={endTurn}>END TURN</Button>
+
             {/*    My Board    */}
             <div className="grid grid-cols-6 gap-2">
                 {cards.map(c => (
